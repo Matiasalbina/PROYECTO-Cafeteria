a22f12babff503b6df6edeaf26f9eb1c8482be34
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2025-07-15 15:49:42 -0400

    CAF-2 elimina comentarios innecesarios en server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,19 +7,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-// Llama a las funciones para crear la base de datos y las tablas al iniciar el servidor
-// (async () => {
-//   try {
-//     await createDatabase(); // Crea la base de datos si no existe
-//     await createTablesAndViews(); // Crea las tablas y vistas en la base de datos "aromacafe"
-//     console.log("Tablas y vistas SQL creadas correctamente.");
-
 app.listen(PORT, async () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-    await createTablesAndViews(); // Crea las tablas y vistas en la base de datos "aromacafe"
-    console.log("Tablas y vistas SQL creadas correctamente.");
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+  await createTablesAndViews(); // Crea las tablas y vistas en la base de datos "aromacafe"
+  console.log("Tablas y vistas SQL creadas correctamente.");
 });
-//   } catch (error) {
-//     console.error("Error al inicializar la base de datos y las tablas:", error);
-//   }
-// })();
